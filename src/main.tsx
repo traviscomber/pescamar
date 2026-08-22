a211761fd7657740a68590e66a2d38fcd13eeb33
traviscomber <n3uralia@gmail.com>
2026-08-22 07:03:19 -0700

    Mount operator authentication provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { AuthProvider } from './auth'
 import './styles.css'
-createRoot(document.getElementById('root')!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>)
+createRoot(document.getElementById('root')!).render(<StrictMode><AuthProvider><BrowserRouter><App /></BrowserRouter></AuthProvider></StrictMode>)
