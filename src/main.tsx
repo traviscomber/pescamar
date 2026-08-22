9b42d787977f20c2028e3059b433b1bb0276ad29
traviscomber <n3uralia@gmail.com>
2026-08-22 09:56:07 -0700

    Load premium login styles last

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,5 @@ import App from './App'
 import { AuthProvider } from './auth'
 import './styles.css'
 import './premium.css'
+import './login-premium.css'
 createRoot(document.getElementById('root')!).render(<StrictMode><AuthProvider><BrowserRouter><App /></BrowserRouter></AuthProvider></StrictMode>)
