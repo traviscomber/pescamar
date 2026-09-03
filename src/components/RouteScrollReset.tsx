ae9a05a1871f5d138015e5ad400fb59f3769f4e0
traviscomber <n3uralia@gmail.com>
2026-09-03 19:18:03 -0400

    chore: remove orphaned route scroll reset

--- src/components/RouteScrollReset.tsx
+++
@@ -1,8 +0,0 @@
-import {useEffect} from 'react'
-import {useLocation} from 'react-router-dom'
-
-export function RouteScrollReset(){
- const {pathname}=useLocation()
- useEffect(()=>{window.scrollTo({top:0,left:0,behavior:'auto'})},[pathname])
- return null
-}
